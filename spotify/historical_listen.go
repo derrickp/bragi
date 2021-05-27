b195335f590af2564360dbd4ba3c52de3996b5a1
Derrick Plotsky <derrickplotsky@gmail.com>
2021-05-26 22:31:27 -0700

    msPlayed is not required
    Apparently it's not always there? Unfortunate.

--- spotify/historical_listen.go
+++ spotify/historical_listen.go
@@ -4,5 +4,5 @@ type HistoricalListen struct {
 	EndTime    string `json:"endTime" binding:"required"`
 	ArtistName string `json:"artistName" binding:"required"`
 	TrackName  string `json:"trackName" binding:"required"`
-	MsPlayed   int    `json:"msPlayed" binding:"required"`
+	MsPlayed   int    `json:"msPlayed"`
 }
